507947dfbe9c115e6e78908378be952816669c77
Tavarley <tomdabunny@hotmail.com>
2013-10-27 14:03:12 +0000

    Added line to keep console window open

--- Hello World/ConsoleApplication1/Program.cs
+++ Hello World/ConsoleApplication1/Program.cs
@@ -10,6 +10,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
         }
     }
 }
