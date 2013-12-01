2dd21cf3abdbf245ca6613692ec1222e0c5f562c
Tavarley <tomdabunny@hotmail.com>
2013-12-01 00:58:58 +0000

    Added some MVA exercises

---
+++ MVAExercises/SwitchEnumsExercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchEnumsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type in a superhero's name to get his nickname");
+            string userValue = Console.ReadLine();
+
+            SuperHero myValue;
+
+            if (Enum.TryParse<SuperHero>(userValue, true, out myValue))
+            {
+                switch (myValue)
+                {
+                    case SuperHero.Batman:
+                        Console.WriteLine("Caped Crusader");
+                        break;
+                    case SuperHero.Superman:
+                        Console.WriteLine("Man of Steel");
+                        break;
+                    case SuperHero.GreenLantern:
+                        Console.WriteLine("Emerald Knight");
+                        break;
+                    default:
+                        Console.WriteLine("Does not compute");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Does not compute");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+    enum SuperHero
+    {
+        Batman,
+        Superman,
+        GreenLantern
+    }
+}
---
+++ MVAExercises/UnderstandingScope/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingScope
+{
+    class Program
+    {
+        private static string k;
+        static void Main(string[] args)
+        {
+            string j = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+             }
+
+            helperMethod();
+            Console.ReadLine();
+
+        }
+
+        private static void helperMethod()
+        {
+            Console.WriteLine("k:" + k);
+        }
+    }
+}
